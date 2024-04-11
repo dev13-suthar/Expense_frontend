4cf89a7c4d002f210bdaa6e3d24a430ec253a753
Dev <dev2005suthar@gmail.com>
2024-04-11 09:05:06 +0530

    UI changes

--- src/scenes/homepage/index.tsx
+++ src/scenes/homepage/index.tsx
@@ -9,6 +9,7 @@ const Homepage = () => {
   return (
     <>
     <div className="text-slate-100 h-[95vh] overflow-scroll">
+      <div className="absolute -top-6.. left-0 h-[50vh] w-full bg-gradient-to-r from-pink-200 to-blue-800 filter opacity-30 -z-10 blur-3xl"/>
           <Header/>
           <AccountOverView/>
           <Divider sx={{
